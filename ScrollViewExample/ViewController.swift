b55646aa688c66d51c32ba3a574f934effaa1e67
Junior Samaroo <jns0987654321@gmail.com>
2017-02-14 23:40:41 -0500

    Finished scroll view with next item preview. TODO: add gesture recognizer to scroll edges

--- ScrollViewExample/ViewController.swift
+++ ScrollViewExample/ViewController.swift
@@ -13,11 +13,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var scrollView: UIScrollView!
     var images = [UIImageView]()
 
+    // cannot access views frame data in view did load
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
         
         var contentWidth: CGFloat = 0.0
+        print("Scrollview width: \(scrollView.frame.size.width)")
+        
+        //Change math to be based on scrollVIew and not the view itself
+        let scrollWidth = scrollView.frame.size.width
         
         for x in 0...2 {
             var image = UIImage(named: "icon\(x).png")
@@ -28,7 +36,7 @@ class ViewController: UIViewController {
             
             // top left corner is 0.0
             // this logic mvoes each image a screen width away from the next
-            newX = view.frame.midX + view.frame.size.width * CGFloat(x)
+            newX = scrollWidth / 2 + scrollWidth * CGFloat(x)
             
             // increase content width to size of image content to scroll
             contentWidth += newX
@@ -37,8 +45,10 @@ class ViewController: UIViewController {
             
             // a views coordinate system dependent on its parent, so we add it after its added to scroll view
             // -75 for half of the image, because coordiante system starts from top left of view
-            imageView.frame = CGRect(x: newX - 75, y: (view.frame.size.height / 2) - 75, width: 150, height: 150)
+            imageView.frame = CGRect(x: newX - 75, y: (scrollView.frame.size.height / 2) - 75, width: 150, height: 150)
         }
+        
+        scrollView.clipsToBounds = false // show dont hide other image being clipped
         
         scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
         
